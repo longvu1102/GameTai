80e125a55cade6d611aded34f77347acf6728c58
vul7896@gmail.com <vul7896@gmail.com>
2023-04-01 21:27:14 +0700

    first commit

--- script.js
+++ script.js
@@ -17,31 +17,58 @@ function playGame() {
   const betAmount = Number(betAmountInput.value);
 
   // Tạo số ngẫu nhiên từ 1 đến 6
-  const randomNumber = Math.floor(Math.random() * 17) ;
-
+  const dice1 = Math.floor(Math.random() * 6) + 1;
+  const dice2 = Math.floor(Math.random() * 6) + 1;
+  const dice3 = Math.floor(Math.random() * 6) + 1;
   // Xác định kết quả Chẵn Lẻ
-  let result;
-  if (randomNumber % 2 === 0) {
-    result = "Chẵn";
-  } else {
-    result = "Lẻ";
+  let result = dice1+dice2+dice3 ;
+  if (result>=11 && result <=17) {
+    result = "Tài";
+  } else if(result>=4 && result<=10) {
+    result = "Xỉu";
   }
   resultText.innerText = `Kết quả: ${result}`;
   resultSection.style.display = "block";
   // Hiển thị kết quả và cập nhật số tiền của người chơi
-  if (result === "Chẵn") {
+  if (result === "Tài") {
     betAmountInput.value = betAmount*2;
     alert("Bạn đã thắng " + betAmount*2);
   } else {
     betAmountInput.value = 0;
     alert("Bạn đã thua " + betAmount);
   }
+  // Reset game sau 5 giây
   
 }
 
 // Hàm reset game
 function resetGame() {
+  // Declare the countdownTimer variable
+  let countdownTimer;
+
+  // Reset giá trị đặt cược và kết quả
   betAmountInput.value = "";
   resultText.innerText = "";
   resultSection.style.display = "none";
+  
+  // Tạm dừng bất kỳ hoạt động nào đang chạy
+  clearInterval(countdownTimer);
+
+  // Bắt đầu đếm ngược lại từ 3
+  let timeLeft = 3;
+  playAgainButton.disabled = true;
+  playAgainButton.innerText = `Chơi lại (${timeLeft}s)`;
+
+  // Assign the countdownTimer variable to setInterval()
+  countdownTimer = setInterval(function() {
+    timeLeft--;
+    if (timeLeft < 0) {
+      clearInterval(countdownTimer);
+      playAgainButton.disabled = false;
+      playAgainButton.innerText = "Chơi lại";
+    } else {
+      playAgainButton.innerText = `Chơi lại (${timeLeft}s)`;
+    }
+  }, 1000);
 }
+
